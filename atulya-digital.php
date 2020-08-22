d448fb599955f0b605bd4afdd505ac5fb8884986
Shubham Kashyap <45028780+shubh010@users.noreply.github.com>
2020-08-22 13:26:59 +0530

    Add files via upload

--- atulya-digital.php
+++ atulya-digital.php
@@ -7,7 +7,7 @@
             <h1 class="uppercase">Combining strategy & <br> design <span>to</span> create brands <br> that lead <span>and</span> not just <br> keep up</h1>
             <div class="content">
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-lg-8">
                         <p class="intro">We design powerful visual identities and usage standards so that your brand’s footprint is consistent and cohesive. From popular brands looking for a change to ambitious start-ups looking to break into the market, we collaborate closely with our clients to create winning brand stories.</p>
 
                     </div>
@@ -21,19 +21,18 @@
     <div class="container">
         <div class="web-wrap">
             <div class="row">
-                <div class="col-xl-5 col-lg-6 col-md-6 order2">
+                <div class="col-xl-6 col-lg-6 col-md-6 order2">
                     <figure>
-                        <img src="assets/img/artist/digital1-min.jpg" class="img-fluid">
+                        <img src="assets/img/artist/digital1-min.png" class="img-fluid">
                     </figure>
                 </div>
 
                 <div class="col-xl-1 view-desktop">
                 </div>
 
-                <div class="col-xl-5 col-lg-6 col-md-6 v-end content order1">
+                <div class="col-xl-3 col-lg-6 col-md-6 j-center content order1">
                     <h3>Brand Identity Design </h3>
-                    <p>What’s in a name? Well, everything!! Giving a name to a new brand or renaming an existing one is the starting point of all your communication. We work closely with our clients to develop a brand name that uniquely expresses the intent of the business. We ensure that the brand name,
-                        messaging & tonality is relevant for your audience and distinguished from your competition.</p>
+                    <p>What’s in a name? Well, everything!! Giving a name to a new brand or renaming an existing one is the starting point of all your communication. We work closely with our clients to develop a brand name that</p>
                 </div>
             </div>
         </div>
@@ -43,14 +42,14 @@
 <section class="atulya-image">
     <div class="container">
         <div class="row">
-            <div class="col-xl-4 col-lg-6 col-md-6 v-end content order1">
+            <div class="col-xl-4 col-lg-6 col-md-6 j-center content order1">
                 <h3>Product Design Graphics</h3>
-                <p>After the logo is finalised we unite it with various branded elements like business cards, stationary, corporate brochures, presentation templates, email signatures, billboards, banners, adverts, packaging, corporate gifts and giveaways. Each brand touchpoint is designed to consistently express the overall brand personality and reiterate the messaging.</p>
+                <p>After the logo is finalised we unite it with various branded elements like business cards, stationary, corporate brochures, presentation templates, email signatures, billboards, banners, adverts, packaging, corporate gifts</p>
             </div>
-            <div class="col-xl-1 view-desktop"></div>
-            <div class="col-xl-7 col-lg-6 col-md-6 order2">
+            
+            <div class="col-xl-8 col-lg-6 col-md-6 order2">
                 <figure>
-                    <img src="assets/img/artist/digital2.jpg" class="img-fluid">
+                    <img src="assets/img/artist/cad-3484213_1920.png" class="img-fluid">
                 </figure>
             </div>
         </div>
@@ -60,7 +59,7 @@
 
 <section class="atulya-image">
     <figure>
-        <img src="assets/img/artist/digital3-min.jpg" class="img-fluid" />
+        <img src="assets/img/artist/pawel-czerwinski-fnLi5j7kPU4-unsplash.png" class="img-fluid w-100" />
     </figure>
 </section>
 
@@ -70,13 +69,13 @@
             <div class="row">
                 <div class="col-xl-1 view-desktop"></div>
                 <div class="col-xl-4 col-lg-6 col-md-6 content order1">
-                    <h3>Marketing & Advertising Graphics</h3>
-                    <p>We understand that a logo is not just the identity of your brand but also the foundation of your marketing strategy. Our design process ensures that we create logos that communicate your brand’s character in the most appealing way. By combining signs, symbols, colours and typeface, we aim to project an image that identifies your product as well as differentiates it from your competitors.</p>
+                    <h3>Packaging design</h3>
+                    <p>We understand that a logo is not just the identity of your brand but also the foundation of your marketing strategy. Our design process ensures that we create logos that communicate your brand’s character in the most appealing way. By</p>
                 </div>
                 <div class="col-xl-6 col-lg-6 col-md-4 order2 p-abs">
                     <figure>
-                        <img src="assets/img/artist/t1.jpg" class="img-fluid f-right">
-                        <img src="assets/img/artist/t2.jpg" class="img-fluid abs-img">
+                        <img src="assets/img/artist/b6bcfb91711527.5e3bd29513a45.png" class="img-fluid f-right">
+                        <img src="assets/img/artist/89489c91711527.5e3b2479d015f.png" class="img-fluid abs-img">
                     </figure>
                 </div>
             </div>
@@ -89,15 +88,15 @@
 <section class="atulya-image">
     <div class="container">
         <div class="row">
-            <div class="col-xl-6 col-lg-6 col-md-6 order2">
+            <div class="col-xl-7 col-lg-6 col-md-6 order2">
                 <figure>
-                    <img src="assets/img/artist/digital5.jpg" class="img-fluid">
+                    <img src="assets/img/artist/sam-mcghee-KieCLNzKoBo-unsplash.png" class="img-fluid w-100">
                 </figure>
             </div>
             <div class="col-xl-1 view-desktop"></div>
-            <div class="col-xl-5 col-lg-6 col-md-6 v-end content order1">
-                <h3>Publication Design Graphics</h3>
-                <p>We understand that a logo is not just the identity of your brand but also the foundation of your marketing strategy. Our design process ensures that we create logos that communicate your brand’s character in the most appealing way. By combining signs, symbols, colours and typeface, we aim to project an image that identifies your product as well as differentiates it from your competitors.</p>
+            <div class="col-xl-3 col-lg-6 col-md-6 j-center content order1">
+                <h3>Film Design</h3>
+                <p>We understand that a logo is not just the identity of your brand but also the foundation of your marketing strategy. Our design process ensures that we create logos that communicate your brand’s character in the most appealing way. By</p>
             </div>
         </div>
     </div>
@@ -105,7 +104,7 @@
 
 <section class="atulya-image">
     <figure>
-        <img src="assets/img/artist/digital6.jpg" class="img-fluid" />
+        <img src="assets/img/artist/pawel-czerwinski-fPN1w7bIuNU-unsplash.png" class="img-fluid w-100" />
     </figure>
 </section>
 
@@ -114,14 +113,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-xl-1 view-desktop"> </div>
-                <div class="col-xl-4 col-lg-6 col-md-6 content order1 j-center">
+                <div class="col-xl-3 col-lg-6 col-md-6 content order1 j-center">
                     <h3>Motion Graphics</h3>
-                    <p>After the logo is finalised we unite it with various branded elements like business cards, stationary, corporate brochures, presentation templates, email signatures, billboards, banners, adverts, packaging, corporate gifts and giveaways. Each brand touchpoint is designed to consistently express the overall brand personality and reiterate the messaging.</p>
+                    <p>After the logo is finalised we unite it with various branded elements like business cards, stationary, corporate brochures, presentation templates, email signatures, billboards, banners, adverts, packaging, corporate gifts</p>
                 </div>
                 <div class="col-xl-1 view-desktop"></div>
                 <div class="col-xl-6 col-lg-6 col-md-4 order2">
                     <figure>
-                        <img src="assets/img/artist/digital7.jpg" class="img-fluid">
+                        <img src="assets/img/artist/graphic-933379_1920.png" class="img-fluid w-100">
                     </figure>
                 </div>
             </div>
@@ -132,18 +131,17 @@
 
 
 <section class="atulya-image abs-img-sec">
-
     <div class="container">
         <div class="row">
             <div class="col-xl-6 col-lg-6 col-md-4 order2 p-abs-l">
                 <figure>
-                    <img src="assets/img/artist/t3.jpg" class="img-fluid">
-                    <img src="assets/img/artist/t4.jpg" class="img-fluid abs-img">
+                    <img src="assets/img/artist/white-printer-paper-196645.png" class="img-fluid">
+                    <img src="assets/img/artist/gui-2311260_1280.png" class="img-fluid abs-img">
                 </figure>
             </div>
-            <div class="col-xl-5 col-lg-6 col-md-6 content order1">
+            <div class="col-xl-3 col-lg-6 col-md-6 content order1">
                 <h3>User Interface Graphics</h3>
-                <p>We understand that a logo is not just the identity of your brand but also the foundation of your marketing strategy. Our design process ensures that we create logos that communicate your brand’s character in the most appealing way. By combining signs, symbols, colours and typeface, we aim to project an image that identifies your product as well as differentiates it from your competitors.</p>
+                <p>We understand that a logo is not just the identity of your brand but also the foundation of your marketing strategy. Our design process ensures that we create logos that communicate your brand’s character in the most appealing way. By</p>
             </div>
         </div>
 
@@ -157,14 +155,14 @@
             <div class="row">
                 <div class="col-xl-1 view-desktop"></div>
                 <div class="col-xl-4 col-lg-6 col-md-6 content order1 j-center">
-                    <h3>Arts & Illustrations</h3>
-                    <p>We understand that a logo is not just the identity of your brand but also the foundation of your marketing strategy. Our design process ensures that we create logos that communicate your brand’s character in the most appealing way. By combining signs, symbols, colours and typeface, we aim to project an image that identifies your product as well as differentiates it from your competitors.
+                    <h3>Digital Art</h3>
+                    <p>We understand that a logo is not just the identity of your brand but also the foundation of your marketing strategy. Our design process ensures that we create logos that communicate your brand’s character in the most appealing way. By
                     </p>
                 </div>
                 <div class="col-xl-1 view-desktop"></div>
                 <div class="col-xl-5 col-lg-6 col-md-4 order2">
                     <figure>
-                        <img src="assets/img/artist/digital8.jpg" class="img-fluid">
+                        <img src="assets/img/artist/charli-xIoA-PC53XA-unsplash.png" class="img-fluid">
                     </figure>
                 </div>
             </div>
