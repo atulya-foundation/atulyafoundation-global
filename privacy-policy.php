b6eccfe11e3ec5ab1b101e1aa209b2aedf25cbe3
Khushboo Sharma <skhushboo1706@gmail.com>
2021-01-17 00:59:13 +0530

    privacy policy and terms of use

--- privacy-policy.php
+++ privacy-policy.php
@@ -16,9 +16,9 @@
 
                 <p>1. Your name</p>
 
-                <p>3. Phone Number</p>
+                <p>2. Phone Number</p>
 
-                <p>4. Email Address</p>
+                <p>3. Email Address</p>
 
                 <p>Above information is for delivery purpose and post-order support only. If you opt to receive our newsletter we might send occasional offers and updates to your email. We do not share this information with any third party, for any purpose whatsoever.</p>
 
@@ -26,7 +26,7 @@
 
                 <h2>IP ADDRESS AND COOKIES</h2>
 
-                <p>We also collect “non-personal” information from you, such as your browser type, the URL of the previous web site you visited, your ISP, operating system, and your IP address. “Non-personal” information cannot be easily used to personally identify you. We may use your non-personal information to help diagnose problems with our server and to administer our website. Your non-personal information may also be used to gather broad demographic information and to recognize customer traffic patterns and website usage. This information aids us in merchandising and in developing the design and layout of the website. We might use 3rd party tools for this purpose.</p>
+                <p>We also collect “non-personal” information from you, such as your browser type, the URL of the previous web site you visited, your ISP, operating system, and your IP address. “Non-personal” information cannot be easily used to personally identify you. We may use your non-personal information to help diagnose problems with our server and to administer our website. Your non-personal information may also be used to gather broad demographic information and to recognize customer traffic patterns and website usage. This information aids us in merchandising and in developing the design and layout of the website. We may use third-party Service Providers to monitor and analyse the use of our website.</p>
 
                 <p>In addition, like almost all e-commerce web sites, we use “cookies.” Cookies are bits of information that are stored on your computer by your web browser. Although our cookies don’t store any of your personal information, they do allow us to do things like keep items in your shopping cart between visits or during the same visit or provide you with a more personalized user experience. Whether you want your web browser to accept cookies or not is up to you. The Help portion of the toolbar on most browsers will tell you how to prevent your browser from accepting new cookies, how to be notified when you receive a new cookie, or how to disable cookies altogether. You can also disable or delete similar data used by browser add-ons, such as Flash cookies, by changing the add-ons settings or visiting the Web site of its manufacturer. However, because cookies allow you to take advantage of some of features and services, we recommend that you leave them turned on. For instance, if you block our cookies, you will not be able to add items to your Cart, proceed to checkout, or use any services that require you to sign in. If you haven’t changed your computer’s settings, most likely your browser already accepts cookies.</p>
 
@@ -42,7 +42,14 @@
 
                 <p>Our services are intended for users ages 18 and older. Accordingly, we will not knowingly collect or use any personal information from persons that we know to be under the age of 18. In addition, we will delete any information in our database that we know originates from a child under the age of 18.</p>
 
+                 <h2>LINKS TO OTHER WEB SITES</h2>
 
+                <p>Our website may contain links to third party web sites or services that are not owned or controlled by Atulya Foundation. Atulya Foundation has no control over, and assumes no responsibility for the content, privacy policies, or practices of any third party web sites or services. We do not warrant the offerings of any of these entities/individuals or their websites.</p>
+                <p>You acknowledge and agree that company shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such third party web sites or services. We strongly advise you to read the terms of service and privacy policies of any third party web sites or services that you visit.</p>
+
+                 <h2>GOVERNING LAW</h2>
+
+                <p>These Terms shall be governed and construed in accordance with the laws of India, which governing law applies to agreement without regard to its conflict of law provisions. Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service and supersede and replace any prior agreements we might have had between us regarding Service.</p>
 
                 <h2>CHANGES TO OUR PRIVACY POLICY</h2>
 
@@ -52,7 +59,7 @@
 
                 <h2>COPYRIGHTS</h2>
 
-                <p>All artwork are exclusive designs of Atulya Foundation, and ATULYA Foundation reserves all rights on the same. Any attempt by an individual or an organisation to copy, or reproduce the paintings or any other artwork on the website, in direct or indirect form – will be subject to severe legal action. We ensure all the paintings and other artwork made by our artists are original artwork, designed by them. If you still believe any design on atulyafoundation.org has been inspired by or copied from existing artist work, please let us know at info@atulyafoundation.org and we shall take appropriate action necessary to ensure no rights are violated.</p>
+                <p>All artwork are exclusive designs of Atulya Foundation, and ATULYA Foundation reserves all rights on the same. Any attempt by an individual or an organisation to copy, or reproduce the paintings or any other artwork on the website, in direct or indirect form – will be subject to severe legal action. We ensure all the paintings and other artwork made by our artists are original artwork, designed by them. If you still believe any design on atulyafoundation.org has been copied from existing artist work, please let us know at info@atulyafoundation.org and we shall take appropriate action necessary to ensure no rights are violated.</p>
 
             </div>
         </div>
