7ba29445a52414bd3ae82b477e627ab002bfeeb1
Khushboo Sharma <skhushboo1706@gmail.com>
2021-01-20 00:48:06 +0530

    footer line

--- arts.php
+++ arts.php
@@ -153,11 +153,11 @@
 <section class="atulya-last j-center container" >
 
     <div class="row">
-        <div class="col-lg-1"></div>
-        <div class="col-lg-10">
-            <p class="medium" style="margin-bottom: -1.5rem;">For any of the above artwork, drop a mail to <a href="mailto:sales@atulyafoundation.org">sales@atulyafoundation.org</a> mentioning name of the painting in the mail subject.</p>
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
+            <p class="medium" style="margin-bottom: -1.5rem;">For any of the above artwork, drop a mail to <a href="mailto:sales@atulyafoundation.org">sales@atulyafoundation.org</a> </p>
         </div>
-        <div class="col-lg-1"></div>
+        <div class="col-lg-3"></div>
     </div>
 
 </section>
--- premium-collection.php
+++ premium-collection.php
@@ -111,11 +111,11 @@
 <section class="atulya-last j-center container" >
 
     <div class="row">
-        <div class="col-lg-1"></div>
-        <div class="col-lg-10">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
             <p class="medium" style="margin-bottom: -1.5rem;">For any of the above products & services, drop a mail to <a href="mailto:sales@atulyafoundation.org">sales@atulyafoundation.org</a> </p>
         </div>
-        <div class="col-lg-1"></div>
+        <div class="col-lg-3"></div>
     </div>
 
 </section>
