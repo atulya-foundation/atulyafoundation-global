267d62c5e855e4835f05d5f4aca8aab4f76b2e5c
Khushboo Sharma <skhushboo1706@gmail.com>
2021-02-01 23:11:15 +0530

    services

--- crafts.php
+++ crafts.php
@@ -1,16 +1,139 @@
 <?php include('header.php') ?>
 
 <style type="text/css">
-    .i .container {
-        margin-left: 12.0625rem;
-        padding-right: 12.0625rem;
+   .parent {
+      position: relative;
+      top: 0;
+      left: 0;
     }
+    .image1 {
+      position: absolute;
+      left: 6.75rem;
+    }
+     .image2 {
+      position: relative;
+      top: 17.5rem;
+      left: 1rem;
+      margin-bottom: 6rem;
+    }
+    .dope{
+        margin-bottom: 12rem;  
+        margin-left: 10.0625rem; 
+        margin-right: 10.0625rem;
+    }
+    .single-image{
+        margin-bottom: 12rem; 
+        margin-left: 4.8rem;
+    }
+    .double-image{
+        margin-left: 4.8rem; 
+        margin-bottom: 18rem;
+    }
+    .large-image{
+        margin-bottom: 12rem;
+    }
+    .bot-line p a {
+        color: #707070;
+        text-decoration: none !important;
+    }
+
+    .bot-line p a:hover {
+        color: black !important;
+    }
+
+    .services h3{
+        font-size: 1.5rem;
+        line-height: 1.75rem;
+        padding-bottom: 1.4125rem;
+        color: #383838;
+        letter-spacing: 0rem;
+        font-family: Roboto, sans-serif;
+        opacity: 1;
+        font-weight: bold;
+    }
+    .services p{
+        font-size: 1.25rem;
+        line-height: 1.5rem;
+        color: #282828;
+        font-family: Roboto;
+        letter-spacing: 0;
+        opacity: 0.8;
+    }
+
+    @media screen and (max-width: 768px){
+        .dope{
+            margin-bottom: 6rem;  
+            margin-left: 0rem; 
+            margin-right: 0rem;
+        }
+        .single-image{
+            margin-bottom: 9rem; 
+            margin-left: 0rem;
+        }
+        .double-image{
+            margin-left: 0rem; 
+            margin-bottom: 0rem;
+        }
+        .large-image{
+            margin-bottom: 12rem;        }
+        .image1 {
+          position: relative;
+          left: -2rem;
+        }
+         .image2 {
+            position: relative;
+            top: -11.5rem;
+            left: 0.625rem;
+            margin-bottom: 0rem;
+        }
+        @media screen and (max-width: 700px){
+            div.images{order: 2;}
+            div.services{order:1;}
+            .single-image{
+                margin-bottom: 9rem;
+            }
+            .large-image{
+            display: none;
+            margin-bottom: 0rem;        
+            }
+            .image1 {
+              position: relative;
+              left: 0;
+            }
+             .image2 {
+                position: relative;
+                top: -11.5rem;
+                left: 2rem;
+                margin-bottom: 0rem;
+            }
+        }
+        
+        .services p{
+            margin-bottom: 4rem;
+        }
+    }
+    @media screen and (width:1024px){
+        .dope{
+            margin-bottom: 6rem;  
+            margin-left: 0rem; 
+            margin-right: 0rem;
+        }
+        .single-image{
+            margin-bottom: 12rem; 
+            margin-left: 0rem;
+        }
+        .double-image{
+            margin-left: 0rem; 
+        }
+    }
+
 </style>
 
 <div class="logo-pos">
     <a href="crafts.php"><img src="assets/img/atulya_crafts_logo.svg" class="img-fluid" alt=""></a>
 </div>
-<section class="main-content dope i">
+
+<section class="main-content dope ">
     <div class="container">
         <h5>Atulya Crafts</h5>
         <div class="web-wrap">
@@ -21,9 +144,6 @@
                         <p class="intro" style="color: #707070;">Our crafts collection contain some of the most beautiful forms of crafts by some of the best craftsmen. We promise
                             to make your living and work space more asthetic and comforting with our craft products. Our craftsmen represent long continuing traditions of
                             craftsmanship who have the magic in their hands to weave beauty into products.</p>
-                        <!-- <div class="touch-div">
-                            <a href="get-in-touch.php" class="touch_link">GET IN TOUCH</a>
-                        </div> -->
                     </div>
                 </div>
             </div>
@@ -31,350 +151,156 @@
     </div>
 </section>
 
-
-<section class="atulya-image abs-img-sec dope">
+<section class="single-image">
     <div class="container">
         <div class="row">
-            <div class="col-xl-1 "></div>
-            <div class="col-xl-6 col-lg-5 col-md-6 order2 p-abs-l new-abs">
-                <figure>
-                    <img src="assets/img/Atulya Crafts/h-s/11.jpg" class="img-fluid">
-                    <img src="assets/img/Atulya Crafts/h-s/12.jpg" class="img-fluid abs-img">
-                </figure>
+            
+            <div class="col-lg-5 col-md-6 images">
+                
+                    <img src="assets/img/Atulya Crafts/Wood crafts/1.jpg" class="img-fluid">
+                
             </div>
-
-            <div class="col-xl-1 col-lg-2 view-desktop"></div>
-
-            <div class="col-xl-4 col-lg-5 col-md-6  content order1 j-center">
-                <h3>Hand-Crafted Stationery</h3>
-                <p>Update your stationery drawer, with our beautiful selection of luxury personalised hand crafted products which are produced with a combination of machines and
-                    handwritten contributions. We create <strong>custom diaries, notebooks, envelopes, papers, cards,</strong> etc. with carefully selected details and finishes
-                    that will aptly represent your brand identity. Let our handcrafted stationery serve as a little reminder of the feeling of putting pen to paper.</p>
-            </div>
-        </div>
-    </div>
-    <div class="clearfix"></div>
-</section>
-
-<section class="atulya-image dope">
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-1 view-desktop"></div>
-            <div class="col-xl-4 col-lg-6 col-md-6 j-center content order1">
+           <div class="col-lg-1 col-md-1"></div>
+            <div class="col-lg-5 col-md-5 services">
                 <h3>Wood Crafts</h3>
                 <p>Wooden handicrafts are renowned for their beauty, durability and utility. Our skilled artisans through their traditional tools carve magnificent designs on
                     wooden items and enhance their look by painting them or with intricate metal/ivory inlay work. Deciduous, evergreen & alpine trees are used extensively to
                     manufacture wooden handicrafts. Wooden handicrafts include <strong>furniture, utensils, boxes, beads, finely carved figurines, accessories</strong> etc.</p>
             </div>
-            <div class="col-xl-1 col-lg-1 view-desktop"></div>
-            <div class="col-xl-6 col-lg-5 col-md-6 order2">
-                <figure>
-                    <img src="assets/img/Atulya Crafts/Wood crafts/1.jpg" class="img-fluid ">
-                </figure>
+            <div class="col-lg-1"></div>
+        </div>
+    </div>
+</section>
+
+
+<section class="double-image">
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-lg-5 col-md-5 services">
+                <h3>Hand-Crafted Stationery</h3>
+                <p>Update your stationery drawer, with our beautiful selection of luxury personalised hand crafted products which are produced with a combination of machines and
+                    handwritten contributions. We create <strong>custom diaries, notebooks, envelopes, papers, cards,</strong> etc. with carefully selected details and finishes
+                    that will aptly represent your brand identity. Let our handcrafted stationery serve as a little reminder of the feeling of putting pen to paper.</p>
+            </div>
+            <div class="col-lg-1 col-md-2 "></div>
+            <div class="col-lg-6 col-md-5 images" class="parent">
+                    <img class="image1 img-fluid" src="assets/img/Atulya Crafts/h-s/11.jpg">
+                    <img class="image2 img-fluid " src="assets/img/Atulya Crafts/h-s/12.jpg">
             </div>
         </div>
     </div>
-    <div class="clearfix"></div>
 </section>
 
-<section class="atulya-image">
+
+
+
+
+
+<section  class="large-image atulya-image">
     <figure>
-        <img src="assets/img/artist/ornaments-4157521_1920.png" class="img-fluid w-100"/>
+        <img src="assets/img/artist/ornaments-4157521_1920.png" class="img-fluid w-100">
     </figure>
 </section>
 
 
-<section class="atulya-image dope">
+<section class="single-image">
     <div class="container">
         <div class="row">
-            <div class="col-xl-1 view-desktop"></div>
-
-            <div class="col-xl-4 col-lg-6 col-md-6 j-center content order1">
+            
+            <div class="col-lg-5 col-md-6 images">
+                
+                    <img src="assets/img/Atulya Crafts/Pottery _ Ceramics/1.jpg" class="img-fluid">
+                
+            </div>
+           <div class="col-lg-1 col-md-1 "></div>
+            <div class="col-lg-5 col-md-5 services">
                 <h3>Pottery & Ceramics Craft</h3>
                 <p>Pottery is considered to be the oldest industry on earth. Ceramic wares are safe to decorate with as they are stone mixed and hence stronger. Our artisans create<strong>
                         flower vase, plant tumblers, terracotta products, tableware, candleholders, lamps, show pieces, pots of various sizes, figurines
                     </strong>and much more. Creating ceramics is a very long and complicated process, requiring lots of patience, attention and knowledge.</p>
             </div>
-            <div class="col-xl-1 view-desktop"></div>
-
-            <div class="col-xl-6 col-lg-6 col-md-6 order2">
-                <figure>
-                    <img src="assets/img/Atulya Crafts/Pottery _ Ceramics/1.jpg" class="img-fluid">
-                </figure>
-            </div>
+            <div class="col-lg-1"></div>
         </div>
     </div>
 </section>
 
 
-<section class="atulya-image abs-img-sec dope">
+<section class="double-image">
     <div class="container">
         <div class="row">
-
-            <div class="col-xl-6 col-lg-5 col-md-6 order2 p-abs dope">
-                <figure>
-                    <img src="assets/img/Atulya Crafts/Metal Crafts/1.jpg" class="img-fluid f-right">
-                    <img src="assets/img/Atulya Crafts/Metal Crafts/2.jpg" class="img-fluid abs-img">
-                </figure>
-            </div>
-            <div class="col-xl-1 col-lg-2 view-desktop"></div>
-            <div class="col-xl-4 col-lg-5 col-md-6  content order1 j-center">
+            
+            <div class="col-lg-5 col-md-5 services">
                 <h3>Metal Crafts</h3>
                 <p>The rare aesthetic beauty and finesse of our metal crafts will leave you wanting even more of it. Items such as <strong>pots, utensils, doorknobs, show pieces,
                         boxes, photo frames, sculptures of deities</strong>, etc. have been crafted by our skilled artisans from iron, copper, silver and alloys like bronze, brass,
                     bell metal, white metal and more. Their elegance and style have left the art lovers simple amazed and enthralled.</p>
             </div>
+            <div class="col-lg-1 col-md-2"></div>
+            <div class="col-lg-6 col-md-5 images" class="parent">
+                    <img class="image1 img-fluid" src="assets/img/Atulya Crafts/Metal Crafts/1.jpg">
+                    <img class="image2 img-fluid" src="assets/img/Atulya Crafts/Metal Crafts/2.jpg">
+            </div>
         </div>
     </div>
-    <div class="clearfix"></div>
 </section>
 
-<section class="atulya-image">
+<section class="large-image atulya-image">
     <figure>
-        <img src="assets/img/Atulya Crafts/bg.jpg" class="img-fluid w-100"/>
+        <img src="assets/img/Atulya Crafts/bg.jpg" class="img-fluid w-100">
     </figure>
 </section>
 
-<section class="atulya-image abs-img-sec dope">
+
+<section class="single-image">
     <div class="container">
         <div class="row">
-            <div class="col-xl-1 view-desktop"></div>
-            <div class="col-xl-6 col-lg-6 col-md-6 order2">
-                <figure>
-                    <img src="assets/img/Atulya Crafts/Glass Crafts/H05pKFg293.jpg" class="img-fluid ">
-                </figure>
+            
+            <div class="col-lg-5 col-md-6 images">
+                
+                    <img src="assets/img/Atulya Crafts/Glass Crafts/H05pKFg293.jpg" class="img-fluid">
+                
             </div>
-            <div class="col-xl-1 col-lg-1 view-desktop"></div>
-            <div class="col-xl-4 col-lg-5 col-md-6 content order1 j-center">
+           <div class="col-lg-1 col-md-1 "></div>
+            <div class="col-lg-5 col-md-5 services">
                 <h3>Glass Crafts</h3>
                 <p>Glass is an incredibly versatile material that can be shaped and formed in unlimited artistic products. Our artisans create glass art by heating glass at a
                     temperature of 2000 degrees or more. The molten glass is then gathered from a furnace and is commonly used to create <strong>sculptures, vases, containers,
                         goblets, art marbles</strong> and other items with techniques such as glass blowing and glass casting.</p>
             </div>
+            <div class="col-lg-1"></div>
         </div>
     </div>
-    <div class="clearfix"></div>
 </section>
 
 
-<section class="atulya-image abs-img-sec dope">
+<section class="double-image">
     <div class="container">
         <div class="row">
-            <div class="col-xl-1"></div>
-            <div class="col-xl-4 col-lg-5 col-md-6 content order1 j-center">
+            
+            <div class="col-lg-5 col-md-5 services">
                 <h3>Stone Crafts</h3>
-                <p>Our artisans work laboriously to craft splendid pieces of stonework. They have been crafting pieces like <strong>toys, decorative items, carved panels, tiles,
-                        paper weights, pen stands, structures & models, sculptures</strong> and many more things. We make our creations on different stones like sandstone, marble,
-                    granite, basalt, graphite etc. Basic design is traced on a stone and is given a crude shape. Final carving is then carried out and items are polished before
-                    delivery.</p>
+                <p>Our artisans work laboriously to craft splendid pieces of stonework. They have been crafting pieces like <strong>toys, decorative items, carved panels, tiles,paper weights, pen stands, structures & models, sculptures</strong> and many more things. We make our creations on different stones like sandstone, marble,granite, basalt, graphite etc. Basic design is traced on a stone and is given a crude shape. Final carving is then carried out and items are polished before delivery.</p>
             </div>
-            <div class="col-xl-1 col-lg-1 view-desktop"></div>
-            <div class="col-xl-6 col-lg-6 col-md-6 order2 p-abs-l">
-                <figure>
-                    <img src="assets/img/Atulya Crafts/Stone Crafts/1.jpg" class="img-fluid">
-                    <img src="assets/img/Atulya Crafts/Stone Crafts/2.jpg" class="img-fluid abs-img">
-                </figure>
+            <div class="col-lg-1 col-md-2"></div>
+            <div class="col-lg-6 col-md-5 images" class="parent">
+                    <img class="image1 img-fluid" src="assets/img/Atulya Crafts/Stone Crafts/1.jpg">
+                    <img class="image2 img-fluid" src="assets/img/Atulya Crafts/Stone Crafts/2.jpg">
             </div>
         </div>
     </div>
-    <div class="clearfix"></div>
 </section>
 
-
-<section class="atulya-last j-center">
+<section class="bot-line">
     <div class="container">
         <div class="row">
-            <div class="col-lg-3"></div>
-            <div class="col-lg-6">
-                <p class="medium" style="margin-bottom: -1.5rem;">For any of the above handicrafts, drop a mail to <a href="mailto:sales@atulyafoundation.org">sales@atulyafoundation.org</a>
-                </p>
-            </div>
+            <div class="col-lg-3 col-md-2"></div>
+            <div class="col-lg-6 col-md-10"><p class="medium" >For any of the above handicrafts, drop a mail to <a href="mailto:sales@atulyafoundation.org">sales@atulyafoundation.org</a>
+                </p></div>
             <div class="col-lg-3"></div>
         </div>
     </div>
 </section>
+
+
 <?php include('footer.php') ?>
-
-<style>
-    .atulya-last p a {
-        color: #707070;
-        text-decoration: none !important;
-    }
-
-    .atulya-last p a:hover {
-        color: black !important;
-    }
-
-    .col,
-    .col-1,
-    .col-10,
-    .col-11,
-    .col-12,
-    .col-2,
-    .col-3,
-    .col-4,
-    .col-5,
-    .col-6,
-    .col-7,
-    .col-8,
-    .col-9,
-    .col-auto,
-    .col-lg,
-    .col-lg-1,
-    .col-lg-10,
-    .col-lg-11,
-    .col-lg-12,
-    .col-lg-2,
-    .col-lg-3,
-    .col-lg-4,
-    .col-lg-5,
-    .col-lg-6,
-    .col-lg-7,
-    .col-lg-8,
-    .col-lg-9,
-    .col-lg-auto,
-    .col-md,
-    .col-md-1,
-    .col-md-10,
-    .col-md-11,
-    .col-md-12,
-    .col-md-2,
-    .col-md-3,
-    .col-md-4,
-    .col-md-5,
-    .col-md-6,
-    .col-md-7,
-    .col-md-8,
-    .col-md-9,
-    .col-md-auto,
-    .col-sm,
-    .col-sm-1,
-    .col-sm-10,
-    .col-sm-11,
-    .col-sm-12,
-    .col-sm-2,
-    .col-sm-3,
-    .col-sm-4,
-    .col-sm-5,
-    .col-sm-6,
-    .col-sm-7,
-    .col-sm-8,
-    .col-sm-9,
-    .col-sm-auto,
-    .col-xl,
-    .col-xl-1,
-    .col-xl-10,
-    .col-xl-11,
-    .col-xl-12,
-    .col-xl-2,
-    .col-xl-3,
-    .col-xl-4,
-    .col-xl-5,
-    .col-xl-6,
-    .col-xl-7,
-    .col-xl-8,
-    .col-xl-9,
-    .col-xl-auto {
-        padding-left: 0;
-        padding-right: 0;
-    }
-
-    .intro {
-        padding-left: 24px !important;
-        padding-right: 0px !important;
-
-    }
-
-    /*.main-content h1 {
-        margin-left: -17px;
-    }*/
-
-    @media only screen and (max-width: 768px) {
-        .col,
-        .col-1,
-        .col-10,
-        .col-11,
-        .col-12,
-        .col-2,
-        .col-3,
-        .col-4,
-        .col-5,
-        .col-6,
-        .col-7,
-        .col-8,
-        .col-9,
-        .col-auto,
-        .col-lg,
-        .col-lg-1,
-        .col-lg-10,
-        .col-lg-11,
-        .col-lg-12,
-        .col-lg-2,
-        .col-lg-3,
-        .col-lg-4,
-        .col-lg-5,
-        .col-lg-6,
-        .col-lg-7,
-        .col-lg-8,
-        .col-lg-9,
-        .col-lg-auto,
-        .col-md,
-        .col-md-1,
-        .col-md-10,
-        .col-md-11,
-        .col-md-12,
-        .col-md-2,
-        .col-md-3,
-        .col-md-4,
-        .col-md-5,
-        .col-md-6,
-        .col-md-7,
-        .col-md-8,
-        .col-md-9,
-        .col-md-auto,
-        .col-sm,
-        .col-sm-1,
-        .col-sm-10,
-        .col-sm-11,
-        .col-sm-12,
-        .col-sm-2,
-        .col-sm-3,
-        .col-sm-4,
-        .col-sm-5,
-        .col-sm-6,
-        .col-sm-7,
-        .col-sm-8,
-        .col-sm-9,
-        .col-sm-auto,
-        .col-xl,
-        .col-xl-1,
-        .col-xl-10,
-        .col-xl-11,
-        .col-xl-12,
-        .col-xl-2,
-        .col-xl-3,
-        .col-xl-4,
-        .col-xl-5,
-        .col-xl-6,
-        .col-xl-7,
-        .col-xl-8,
-        .col-xl-9,
-        .col-xl-auto {
-            padding-left: 15px;
-            padding-right: 30px;
-        }
-
-        .intro {
-            padding-left: 0px !important;
-            padding-right: 0px !important;
-
-        }
-
-        .dope h1 {
-            font-size: 2.775rem;
-            margin-top: 0rem;
-            margin-bottom: 1rem;
-        }
-    }
-
-</style>
