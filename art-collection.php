508797b79bf821df76476a1c4da62d3c8d2fd55c
Khushboo Sharma <skhushboo1706@gmail.com>
2021-07-31 18:08:37 +0530

    add art-collection

---
+++ art-collection.php
@@ -0,0 +1,287 @@
+<?php include('header.php') ?>
+
+
+<div class="logo-pos">
+    <a href="arts"><img src="assets/img/atulya_arts_logo.svg" class="img-fluid" alt=""></a>
+</div>
+
+<section class="main-content dope">
+    <div class="container">
+        <h5>Art Collection</h5>
+        <div class="web-wrap">
+            <h1 class="uppercase">PAINTING <span>is</span> JUST ANOTHER<br>WAY <span>of</span> KEEPING A DIARY</h1>
+            <div class="content">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p class="intro" style="color: #707070;">Our art collection contains paintings of different themes and genres. We try to inculcate as many art forms as
+                            possible in our collection. Art lovers are free to choose according to their own interests and preferences.</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="atulya-image premium dope">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-6  content">
+                <figure>
+                    <img src="assets/img/artist/LRM_EXPORT_20180812_175443-01.png" class="img-fluid " alt="">
+                </figure>
+                <h3>Wealth of Contentment</h3>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <p>According to Mahatma Buddha contentment is the biggest wealth and this girl seems to have realized this, maybe not in the same language, at a very young
+                            age. Poverty cannot steal her happiness and her innocent smile depicts this in a very beautiful manner. This painting has been made keeping in mind the
+                            intensity of this emotion and has done so with its unique combination of colours.</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6  content">
+                <figure>
+                    <img src="assets/img/artist/DSC_0981-01.png" class="img-fluid " alt="">
+                </figure>
+                <h3>Awakening</h3>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <p>A rising sun symbolizes new energy and awakening. A mother takes her daughter from deep dark woods towards the bright rising sun which represents
+                            empowerment. This painting represents a bright future of the society which can only come with a bright future of the women. Like nature which nurtures
+                            all the creatures, a woman nurtures her family and this painting depicts this in its own colourful and subtle language.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="atulya-image premium dope">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <figure>
+                    <img src="assets/img/artist/DSC_0992-01.png" class="img-fluid w-100"/>
+                </figure>
+            </div>
+            <div class="col-xl-4 content" style="flex: 100%!important; max-width: 100%!important;">
+                <h3>Sacred Symphony of Love</h3>
+                <p>Radha and Krishna are the epitome of divine love and this painting tries to depict it in its own unique and creative way. The beautiful and unique intermixing of
+                    various colours represents the coming together of two souls. The symphony of flute represents emotions which forms the source of sacred union of Radha- Krishna.
+                    The painting defines the purest form of love in its own language of colours.</p>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+<section class="atulya-image premium dope">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-6  content">
+                <figure>
+                    <img src="assets/img/artist/l1.jpg" class="img-fluid " alt="">
+                </figure>
+                <h3>Tazurba</h3>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <p>Experience or Tazurba is the most important treasure of life and the man in the painting does not need to speak that he has it. The intense look, long
+                            white beard and rural outlook of this person tell us that he has so many stories to tell and so many lessons to teach and you certainly won’t learn them
+                            in your schools. The painting, with its unique theme, represents one of the most important class of individuals of our society.</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6  content">
+                <figure>
+                    <img src="assets/img/artist/a.jpg" class="img-fluid " alt="">
+                </figure>
+                <h3>Panchtatva</h3>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <p>The Indian philosophy believes that the world is made up of five elements land, water, sky, air and fire and everything around us is a manifestation of
+                            these elements in some form. While the feeling of being a human seems to be very strong and special, we should not forget that we have come from these
+                            elements and will go back to them. This painting, with a very beautiful colour scheme, portrays the most important philosophy, or law, of nature.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="atulya-image premium dope">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-6  content">
+                <figure>
+                    <img src="assets/img/artist/DSC_0985-01.png" class="img-fluid " alt="">
+                </figure>
+                <h3>Bond of Togetherness</h3>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <p>This painting, in its very unique colour style, has tried to depict the bond of togetherness. The foundation of a society is togetherness and one of the
+                            most beautiful forms of togetherness is that of a brother and sister. From care free childhood plays to growing up into bond of responsibilities, the
+                            brother-sister relationship is a gift of life and this painting has tried to put it beautifully on a canvass.</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6  content">
+                <figure>
+                    <img src="assets/img/artist/Interface.jpg" class="img-fluid " alt="">
+                </figure>
+                <h3>Interface</h3>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <p>The interaction of human and nature is the essence of existence. We are part of the nature and yet are different in our own form. This painting has tried
+                            to showcase this beautiful thought of human-nature interface. This painting uses a very unique colour scheme to bring out an artist’s imagination of
+                            human being as a part of nature.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="atulya-image premium dope">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <figure>
+                    <img src="assets/img/artist/z.jpg" class="img-fluid w-100"/>
+                </figure>
+            </div>
+            <div class="col-xl-4 content" style="flex: 100%!important; max-width: 100%!important;">
+                <h3>Hope</h3>
+                <p>When everything else seems to be lost, hope remains. With its unique style and colour scheme, this painting portrays the emotion of hope. From humans beings to
+                    animals, hope as an emotion is present in everyone in different forms and is expressed in different languages. Hope represents optimism and belief over a divine
+                    force. The cute duo in the painting, lost in the vastness of water, hopes to find a shore.</p>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+<section class="atulya-last j-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6">
+                <p class="medium" style="margin-top:6vh;margin-bottom: -2.5vh;">For any of the above artwork, drop a mail to <a href="mailto:sales@atulyafoundation.org">sales@atulyafoundation.org</a>
+                </p>
+            </div>
+            <div class="col-lg-3"></div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php') ?>
+
+<style type="text/css">
+    .atulya-last p a {
+        color: #707070;
+        text-decoration: none !important;
+    }
+
+    .atulya-last p a:hover {
+        color: black !important;
+    }
+
+    /* .dope .container {
+        margin-left: 14.5rem;
+        padding-right: 12.0625rem;
+    } */
+
+    @media only screen and (max-width: 768px) {
+        .main-content {
+            margin-top: 6.125rem !important;
+        }
+
+        .col,
+        .col-1,
+        .col-10,
+        .col-11,
+        .col-12,
+        .col-2,
+        .col-3,
+        .col-4,
+        .col-5,
+        .col-6,
+        .col-7,
+        .col-8,
+        .col-9,
+        .col-auto,
+        .col-lg,
+        .col-lg-1,
+        .col-lg-10,
+        .col-lg-11,
+        .col-lg-12,
+        .col-lg-2,
+        .col-lg-3,
+        .col-lg-4,
+        .col-lg-5,
+        .col-lg-6,
+        .col-lg-7,
+        .col-lg-8,
+        .col-lg-9,
+        .col-lg-auto,
+        .col-md,
+        .col-md-1,
+        .col-md-10,
+        .col-md-11,
+        .col-md-12,
+        .col-md-2,
+        .col-md-3,
+        .col-md-4,
+        .col-md-5,
+        .col-md-6,
+        .col-md-7,
+        .col-md-8,
+        .col-md-9,
+        .col-md-auto,
+        .col-sm,
+        .col-sm-1,
+        .col-sm-10,
+        .col-sm-11,
+        .col-sm-12,
+        .col-sm-2,
+        .col-sm-3,
+        .col-sm-4,
+        .col-sm-5,
+        .col-sm-6,
+        .col-sm-7,
+        .col-sm-8,
+        .col-sm-9,
+        .col-sm-auto,
+        .col-xl,
+        .col-xl-1,
+        .col-xl-10,
+        .col-xl-11,
+        .col-xl-12,
+        .col-xl-2,
+        .col-xl-3,
+        .col-xl-4,
+        .col-xl-5,
+        .col-xl-6,
+        .col-xl-7,
+        .col-xl-8,
+        .col-xl-9,
+        .col-xl-auto {
+            padding-left: 15px;
+            padding-right: 30px;
+        }
+
+        .dope h1 {
+            font-size: 2.775rem;
+            margin-top: 0rem;
+            margin-bottom: 1rem;
+        }
+
+        .premium .content p {
+            font-size: 1rem !important;
+        }
+    }
+</style>
